9e1c95a6cd003b9653a627e3ce0ee0d2df938b88
tbelk12 <taylor.belk@americannational.com>
2019-11-12 08:32:13 -0600

    same as last

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import Nav from './nav/nav';
-import MainCard from './mainCard/mainCard';
-import { MainWrap } from './styles/mainStyles';
-import Avatar from '../components/avatar/avatar';
-import Footer from '../components/footer/footer';
-import FindMore from '../gtkmButton/button';
 import { Route } from 'react-router-dom';
 
 const Main = () => {
