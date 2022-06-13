be684dbfe6c70bf3306b164ab8cf8dbddddbe973
Taylorbelk1 <belktaylor12@gmail.com>
2022-06-13 10:52:10 -0500

    updated verbiage in about me

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import {
-    AboutMeWrapper,
-    ContentWrap,
-    ByLine
-} from './styles/aboutMeStyles';
-
+import React from "react";
+import { AboutMeWrapper, ContentWrap, ByLine } from "./styles/aboutMeStyles";
 
 const AboutMe = () => {
-    return (
-        <AboutMeWrapper>
-            <ContentWrap>
-                <ByLine>Hey there, I'm Taylor!</ByLine>
-                <p>I am a Full-Stack Web Developer. I have a three years of experience creating web based applications using technologies such as ReactJS, Angular 2+, Bootstrap, Material UI, NodeJS, Express, SQL, GraphQL, PostGres, and even a bit of Java! I pick up new technologies very quickly and enjoy constantly challenging myself.</p>
-            </ContentWrap>
-        </AboutMeWrapper>
-    )
-}
+  return (
+    <AboutMeWrapper>
+      <ContentWrap>
+        <ByLine>Hey there, I'm Taylor!</ByLine>
+        <p>
+          I am a Full-Stack Web Developer. Since 2018 I have been creating web
+          based applications using technologies such as ReactJS, Angular 2+,
+          Bootstrap, Material UI, NodeJS, Express, NestJS, SQL, GraphQL,
+          PostGres, and Java. I pick up new technologies very quickly and enjoy
+          constantly challenging myself.
+        </p>
+      </ContentWrap>
+    </AboutMeWrapper>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
